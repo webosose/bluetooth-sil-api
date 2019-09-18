cab140730247fb40b51bacb712e70d689d494548
sameer.mulla <sameer.mulla@lge.com>
2019-09-18 20:31:17 +0530

    Added method to notify status of multiple adapters
    :Release Notes:
    New apis added in a2dp and profile class
    
    :Detailed Notes:
    To support multiple adapters
    a2dp state changed new api added with adapterAddress
    profile properties changed new api with adapterAddresss
    
    :Testing Performed:
    Builded and Tested
    
    :QA Notes:
    
    :Issues Addressed:
    [PLAT-85821] Implement a2dp/connect supporting multiple Adapters
    
    Change-Id: I2c1ace846e101c3746671fa663f8550d33e8e7bf
    Reviewed-on: http://gpro.lge.com/256416
    Reviewed-by: Sameer Mulla <sameer.mulla@lge.com>
    Tested-by: Sameer Mulla <sameer.mulla@lge.com>
    Reviewed-by: Sathya Krishnan <sathya.krishnan@lge.com>
    Reviewed-by: Rajesh Kanna <rajesh.kanna@lge.com>

--- include/public/bluetooth-sil-api/a2dp.h
+++ include/public/bluetooth-sil-api/a2dp.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2018 LG Electronics, Inc.
+// Copyright (c) 2015-2019 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -324,6 +324,14 @@ public:
 	 * @param state current playing state of the A2DP profile.
 	*/
 	virtual void stateChanged(std::string address, BluetoothA2dpProfileState state) { }
+
+	/**
+	 * @brief The method is called when the profile state is changed
+	 * @param adapterAddress address of adapter
+	 * @param address Address of the device
+	 * @param state current playing state of the A2DP profile.
+	*/
+	virtual void stateChanged(std::string adapterAddress, std::string address, BluetoothA2dpProfileState state) { }
 
 	/**
 	 * @brief The method is called when A2DP audio socket is created
