71bb8ca1e2b55bbf51aac6fa6f0eba7ac626eda4
Rakes Pani <rakes.pani@lge.com>
2020-07-22 17:45:46 +0530

    MAP Client support for getMessageList API
    :Release Notes:
    MAP getMessageList API implementation
    
    :Detailed Notes:
    Implementation of getMessageList API to get the
    messages.
    
    :Testing Performed:
    Built and tested using luna command:
    luna-send -n 1 -f luna://com.webos.service.bluetooth2/map/
    getMessageList '{"adapterAddress": "b8:27:eb:e6:5b:e5",
    "address": "d4:63:c6:27:11:23","sessionId":"session6","folder":"sent",
    "filter":{"maxCount":1}}'
    {
        "instanceName": "SMS/MMS",
        "adapterAddress": "b8:27:eb:e6:5b:e5",
        "address": "d4:63:c6:27:11:23",
        "returnValue": true,
        "messages": [
            {
                "properties": {
                    "status": "complete",
                    "priority": false,
                    "recipientName": "",
                    "type": "sms-gsm",
                    "read": true,
                    "subject": "Oh ok",
                    "recipientAddress": "1234567890",
                    "textType": true,
                    "sent": true,
                    "senderName": "",
                    "protected": false,
                    "senderAddress": "",
                    "dateTime": "20200717T121528",
                    "folder": "/telecom/msg/sent"
                },
                "handle": "message288230376151727272"
            }
        ]
    }
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [PLAT-115154] Implement map/getMessageList in SIL-Bluez5
    
    Change-Id: Iba4338d2efa9dae32d2bb31afb367bb11dd85b74
    (cherry picked from commit cc5038c8ab47babd3efd64288c85acc9b28069fb)

--- include/public/bluetooth-sil-api/map.h
+++ include/public/bluetooth-sil-api/map.h
@@ -37,6 +37,7 @@ typedef std::function<void(BluetoothError, std::list<std::string>filters)>
 BluetoothMapListFiltersResultCallback;
 
 typedef std::function<void(BluetoothError, std::vector<std::string>&)>BluetoothMapGetFoldersCallback;
+
 /**
  * @brief This interface is the base to implement an observer for the MAP profile.
  */
@@ -50,6 +51,293 @@ public:
 	 */
 	virtual void profilePropertiesChanged(const std::string &adapterAddress, const std::string &address) { }
 };
+
+/**
+ * @brief The BluetoothMapProperty class abstracts several properties of message or filters of getMessageList.
+ */
+class BluetoothMapProperty
+{
+public:
+	enum Type
+	{
+		/**
+		 * @brief Property does not contain any information.
+		 **/
+		EMPTY,
+		/**
+		 * @brief start offSet for getMessageList.
+		 *
+		 *        Type: uint16_t
+		 *        Access: Service (write) , SIL(Read)
+		 **/
+		STARTOFFSET,
+		/**
+		 * @brief maxnumber of messages needs to be downloaded for getMessageList.
+		 *
+		 *        Type: uint16_t
+		 *        Access: Service (write) , SIL(Read)
+		 **/
+		MAXCOUNT,
+		/**
+		 * @brief Folder which the message belongs to.
+		 *
+		 *        Type: sting
+		 *        Access: Service (Read) , SIL(write)
+		 **/
+		FOLDER,
+		/**
+		 * @brief max length of subject , used in getMessageList.
+		 *
+		 *        Type: uint8_t
+		 *        Access: Service (write) , SIL(Read)
+		 **/
+		SUBJECTLENGTH,
+		/**
+		 * @brief message fields those needs to be downloaded for getMessageList.
+		 *
+		 *        Type: std::vector<std::string>
+		 *        Access: Service (write) , SIL(Read)
+		 **/
+		FIELDS,
+		/**
+		 * @brief Message Types needs to be downloaded , used in getMessageList.
+		 *
+		 *        Type: std::vector<std::string>
+		 *        Access: Service (write) , SIL(Read)
+		 **/
+		MESSAGETYPES,
+		/**
+		 * @brief Period from which messages needs to be downloaded , used in getMessageList.
+		 *
+		 *        Type: std::string
+		 *        Access: Service (write) , SIL(Read)
+		 **/
+		PERIODBEGIN,
+		/**
+		 * @brief Period till which messages needs to be downloaded , used in getMessageList.
+		 *
+		 *        Type: std::string
+		 *        Access: Service (write) , SIL(Read)
+		 **/
+		PERIODEND,
+		/**
+		 * @brief Message status . It is also used as filter for getMessageList
+		 *
+		 *        Type: Boolean
+		 *        Access: Service (Read/write) , SIL(Read/write)
+		 **/
+		READ,
+		/**
+		 * @brief Message recipient name. It is also used as filter in getMessageList
+		 *
+		 *        Type: std::string
+		 *        Access: Service (Read/write), SILs(Read/write)
+		 **/
+		RECIPIENT,
+		/**
+		 * @brief Message recipient address.
+		 *
+		 *        Type: Boolean
+		 *        Access: Service (Read) , SIL(write)
+		 **/
+		RECIPIENTADDRESS,
+		/**
+		 * @brief Message sender name. It is also used as filter in getMessageList.
+		 *
+		 *        Type: std::string
+		 *        Access: Service (Read/write), SILs(Read/write)
+		 **/
+		SENDER,
+		/**
+		 * @brief Message sender address.
+		 *
+		 *        Type: Boolean
+		 *        Access: Service (Read) , SIL(write)
+		 **/
+		SENDERADDRESS,
+		/**
+		 * @brief Message Reply-To address.
+		 *
+		 *        Type: std::string
+		 *        Access: Service (Read), SIL(write)
+		 **/
+		REPLYTO,
+		/**
+		 * @brief Message priority flag. It is also used as filter in getMessageList
+		 *
+		 *        Type: Boolean
+		 *        Access: Service (Read/write) , SIL(Read/write)
+		 **/
+		PRIORITY,
+		/**
+		 * @brief Message subject.
+		 *
+		 *        Type: Boolean
+		 *        Access: Service (Read) , SIL(write)
+		 **/
+		SUBJECT,
+		/**
+		 * @brief Message timestamp , indicates when the message is sent or received.
+		 *
+		 *        Type: std::string
+		 *        Access: Service (Read) , SIL(write)
+		 **/
+		TIMESTAMP,
+		/**
+		 * @brief Message size in bytes.
+		 *
+		 *        Type: uint64_t
+		 *        Access: Service (Read) , SIL(write)
+		 **/
+		SIZE,
+		/**
+		 * @brief Message text flag.
+		 *
+		 *        Type: boolean
+		 *        Access: Service (Read) , SIL(write)
+		 **/
+		TEXTTYPE,
+		/**
+		 * @brief Message status.
+		 *
+		 *        Type: std::string
+		 *        Access: Service (Read) , SIL(write)
+		 **/
+		STATUS,
+		/**
+		 * @brief Message overall attachment size in bytes.
+		 *
+		 *        Type: uint64_t
+		 *        Access: Service (Read) , SIL(write)
+		 **/
+		ATTACHMENTSIZE,
+		/**
+		 * @brief Message protected flag.
+		 *
+		 *        Type: boolean
+		 *        Access: Service (Read) , SIL(write)
+		 **/
+		PROTECTED,
+		/**
+		 * @brief Message sent flag.
+		 *
+		 *        Type: boolean
+		 *        Access: Service (Read) , SIL(write)
+		 **/
+		SENT
+	};
+
+	/**
+	 * @brief Initialize an empty property
+	 *
+	 *        The type of the property will be set to EMPTY.
+	 */
+	BluetoothMapProperty() :
+		type(EMPTY),
+		impl(0)
+	{
+	}
+
+	/**
+	 * @brief Initialize the property with a type
+	 * @param type Type of the property
+	 */
+	BluetoothMapProperty(Type type) :
+		type(type),
+		impl(0)
+	{
+	}
+
+	/**
+	 * @brief Copy constructor
+	 * @param other Other property to create a copy from
+	 */
+	BluetoothMapProperty(const BluetoothMapProperty &other) :
+		type(other.type),
+		impl(other.impl),
+		typeName(other.typeName)
+	{
+	}
+
+	/**
+	 * @brief Initialize the property with a type and a value
+	 * @param type Type of the property
+	 * @param value Value of the property
+	 */
+	template<class T>
+	BluetoothMapProperty(Type type, T value) :
+		type(type),
+		impl(new PropertyImpl<T>(value)),
+		typeName(typeid(T).name()) { }
+
+	/**
+	 * @brief Get the type of the property
+	 * @return Type of the property
+	 */
+	Type getType() const
+	{
+		return type;
+	}
+
+	/**
+	 * @brief Get the value of the property
+	 * @return Value of the property
+	 */
+	template<class T>
+	T getValue() const
+	{
+		std::string requestedType = typeid(T).name();
+
+		if (requestedType != typeName)
+		{
+			throw std::logic_error("Non-matching types");
+		}
+
+		return dynamic_cast<PropertyImpl<T>*>(impl.get())->getValue();
+	}
+
+	/**
+	 * @brief Set the value of the property
+	 * @param value New value of the property
+	 */
+	template<class T>
+	void setValue(T value)
+	{
+		impl.reset(new PropertyImpl<T>(value));
+		typeName = typeid(T).name();
+	}
+
+private:
+	struct BasePropertyImpl
+	{
+		virtual ~BasePropertyImpl() { }
+	};
+
+	template<class T>
+	struct PropertyImpl : public BasePropertyImpl
+	{
+		PropertyImpl(T value) : value(value) { }
+		~PropertyImpl() { }
+
+		T getValue() const
+		{
+			return value;
+		}
+
+		T value;
+	};
+
+private:
+	Type type;
+	std::shared_ptr<BasePropertyImpl> impl;
+	std::string typeName;
+};
+
+typedef std::vector<BluetoothMapProperty> BluetoothMapPropertiesList;
+
+typedef std::vector<std::pair<std::string, BluetoothMapPropertiesList>> BluetoothMessageList;
+
+typedef std::function<void(BluetoothError, BluetoothMessageList &)>BluetoothMapGetMessageListCallback;
 
 /**
  * @brief Interface to abstract the operations for the MAP bluetooth profile.
@@ -126,9 +414,21 @@ public:
 	 * @param sessionId Unique identifier of a session which was returned from map/connect.
 	 * @param folder name of the folder or path to the folder.
 	 * @param callback Callback function which is called when the operation is done or
-	 *		  has failed.
+	 *        has failed.
 	 */
 	 virtual void setFolder(const std::string &sessionKey, const std::string &sessionId, const std::string &folder, BluetoothResultCallback callback) = 0;
+
+	/**
+	 * @brief To Get Message list of a connected MAS session.
+	 *
+	 * @param sessionKey Unique identifier of connected MAP insatnce(address_instanceName)
+	 * @param sessionId Unique identifier of a session which was returned from map/connect.
+	 * @param folder name of the folder or path to the folder.
+	 * @param filters list of filters applicable for this application.
+	 * @param callback Callback function which is called when the operation is done or
+	 *        has failed.
+	 */
+	 virtual void getMessageList(const std::string &sessionKey, const std::string &sessionId, const std::string &folder, const BluetoothMapPropertiesList &filters, BluetoothMapGetMessageListCallback callback) = 0;
 
 protected:
 	/**
@@ -154,5 +454,4 @@ private:
 	BluetoothMapStatusObserver *observer;
 
 };
-
 #endif //BLUETOOTH_SIL_MAP_H_
